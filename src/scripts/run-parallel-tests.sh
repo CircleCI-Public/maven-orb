d162496d214b9d32116a13bdd5886dc09d69188f
Jaryt <jarytbustard@gmail.com>
2022-10-11 12:21:31 -0400

    chore: test clean install pre step

--- src/scripts/run-parallel-tests.sh
+++ src/scripts/run-parallel-tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 if [ -n "" ]; then
-    set -x
     set -- "$@" --settings "$PARAM_SETTINGS_FILE"
-    set +x
 fi
+
+set -x
 "$PARAM_MVN_COMMAND" "$PARAM_COMMAND" -Dsurefire.excludesFile=.circleci/tests/surefire_classnames_ignore_list -Dfailsafe.excludesFile=.circleci/tests/failsafe_classnames_ignore_list "$@"
+set +x
--- src/scripts/run-tests.sh
+++ src/scripts/run-tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 if [ -n "$PARAM_SETTINGS_FILE" ]; then
-    set -x
     set -- "$@" --settings "$PARAM_SETTINGS_FILE"
-    set +x
 fi
+
+set -x
 "$PARAM_MVN_COMMAND" "$PARAM_COMMAND" "$@"
+set +x

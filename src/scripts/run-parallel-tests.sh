c47b15ba535bc8892231c411914289cb03262461
Jaryt Bustard <jarytbustard@gmail.com>
2022-10-11 12:05:53 -0400

    feat: verbose argument setting
    Co-authored-by: Kyle a.k.a. TechSquidTV <33272306+KyleTryon@users.noreply.github.com>

--- src/scripts/run-parallel-tests.sh
+++ src/scripts/run-parallel-tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 if [ -n "" ]; then
+    set -x
     set -- "$@" --settings "$PARAM_SETTINGS_FILE"
+    set +x
 fi
 "$PARAM_MVN_COMMAND" "$PARAM_COMMAND" -Dsurefire.excludesFile=.circleci/tests/surefire_classnames_ignore_list -Dfailsafe.excludesFile=.circleci/tests/failsafe_classnames_ignore_list "$@"

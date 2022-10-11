7950514a88d71e50ced1f9e34f4bc7d73113aa04
Jaryt <jarytbustard@gmail.com>
2022-10-11 13:05:56 -0400

    fix: settings file not being used by verify script

--- src/scripts/verify.sh
+++ src/scripts/verify.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+if [ -n "$PARAM_SETTINGS_FILE" ]; then
+    set -- "$@" --settings "$PARAM_SETTINGS_FILE"
+fi
+
+set -x
 $PARAM_MVN_COMMAND org.apache.maven.plugins:maven-dependency-plugin:3.1.2:go-offline "$@"
+set +x

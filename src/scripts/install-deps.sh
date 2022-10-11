7aa7b6cd390ae7ccde3a513b27a566011b4d8196
Jaryt <jarytbustard@gmail.com>
2022-10-11 12:53:40 -0400

    chore: reverted unnecessary fixes for later

---
+++ src/scripts/install-deps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -n "$PARAM_SETTINGS_FILE" ]; then
+    set -x
+    set -- "$@" --settings "$PARAM_SETTINGS_FILE"
+    set +x
+fi
+
+"$PARAM_MVN_COMMAND" "$PARAM_COMMAND" "$@"
--- src/scripts/verify.sh
+++ src/scripts/verify.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if [ -n "$PARAM_SETTINGS_FILE" ]; then
-    set -- "$@" --settings "$PARAM_SETTINGS_FILE"
-fi
-
-$PARAM_MVN_COMMAND org.apache.maven.plugins:maven-dependency-plugin:"$PARAM_DEP_PLUGIN_VER":go-offline "$@"
+$PARAM_MVN_COMMAND org.apache.maven.plugins:maven-dependency-plugin:3.1.2:go-offline "$@"

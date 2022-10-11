e307e85a77eeadb3018c42e48e172e1b01ca5c29
Jaryt Bustard <jarytbustard@gmail.com>
2022-10-11 14:27:36 -0400

    feat: parameterize dependency plugin version (#31)

--- src/scripts/verify.sh
+++ src/scripts/verify.sh
@@ -4,5 +4,5 @@ if [ -n "$PARAM_SETTINGS_FILE" ]; then
 fi
 
 set -x
-$PARAM_MVN_COMMAND org.apache.maven.plugins:maven-dependency-plugin:3.1.2:go-offline "$@"
+$PARAM_MVN_COMMAND org.apache.maven.plugins:maven-dependency-plugin:"$PARAM_DEP_PLUGIN_VER":go-offline "$@"
 set +x

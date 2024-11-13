86f9e964713ffbad0273b8771ff963d649fdb6ac
Peter Darton <97306111+Peter-Darton-i2@users.noreply.github.com>
2024-11-13 13:55:57 +0000

    Bugfix and DRY out code (#46)
    * Fix bug whereby parallel_test ignores $PARAM_SETTINGS_FILE.
    
    Refactor: Made parallel-specific functionality part of generic run-tests.sh,
    made parallel_test job use run-tests.sh instead
    ... which doesn't have the bug.
    
    * Tidy up sample_apps
    
    * Use sample_app not sample_app2 for all testing
    
    * Handle non-standard test result location too
    
    * Set file attributes
    
    ---------
    
    Co-authored-by: marboledacci <mateo.arboleda@circleci.com>

--- sample_app/src/test/java/com/mycompany/app/AppTest.java
+++ sample_app/src/test/java/com/mycompany/app/AppTest.java
@@ -52,33 +52,5 @@ public class AppTest
     {
         assertTrue( true );
     }
-
-    @Test
-    public void shouldEqualFive()
-    {
-        float correctAnswer = 5;
-        assertEquals(correctAnswer, Calculator.Addition(2,3), 0.000001);
-    }
-
-    @Test
-    public void shouldEqualThree()
-    {
-        float correctAnswer = 3;
-        assertEquals(correctAnswer, Calculator.Subtraction(5, 2), 0.000001);
-    }
-
-    @Test
-    public void shouldEqualTwentyOne()
-    {
-        float correctAnswer = 21;
-        assertEquals(correctAnswer, Calculator.Multiplication(7, 3), 0.000001);
-    }
-
-    @Test
-    public void DivisionTest()
-    {
-        float correctAnswer = 0.41666f;
-        assertEquals(correctAnswer, Calculator.Division(5, 12), 0.00001);
-    }
 }
 
--- src/scripts/run-parallel-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -n "" ]; then
-    set -- "$@" --settings "$PARAM_SETTINGS_FILE"
-fi
-
-set -x
-"$PARAM_MVN_COMMAND" "$PARAM_COMMAND" -Dsurefire.excludesFile=.circleci/tests/surefire_classnames_ignore_list -Dfailsafe.excludesFile=.circleci/tests/failsafe_classnames_ignore_list "$@"
-set +x

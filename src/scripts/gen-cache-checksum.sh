52d4a40eebb0a8fdcc1e61cd8a2a69e47a9acf3a
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-07-08 14:24:40 -0500

    Fix behavior of root directory on windows

--- src/scripts/gen-cache-checksum.sh
+++ src/scripts/gen-cache-checksum.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
-if [[ "$PARAM_CHECKSUM_DIR" == /* ]]; then 
-    CHECKSUM_DIR="$PARAM_CHECKSUM_DIR"
+detect_os() {
+  detected_platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
+
+  case "$detected_platform" in
+    linux*)
+      PLATFORM=linux
+      ;;
+    darwin*)
+      PLATFORM=macos
+      ;;
+    msys*|cygwin*)
+      PLATFORM=windows
+      ;;
+    *)
+      printf '%s\n' "Unsupported OS: \"$detected_platform\"."
+      exit 1
+      ;;
+  esac
+}
+
+detect_os
+
+if [[ "$PARAM_CHECKSUM_DIR" == /* ]]; then
+    if [[ "$PLATFORM" == "windows" ]]; then
+        CHECKSUM_DIR="/c$PARAM_CHECKSUM_DIR"
+    else
+        CHECKSUM_DIR="$PARAM_CHECKSUM_DIR"
+    fi
+
 else
     CHECKSUM_DIR="${PWD%/"$PARAM_APP_SRC_DIR"}/$PARAM_CHECKSUM_DIR"
 fi

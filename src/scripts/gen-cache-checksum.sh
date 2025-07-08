b08c50f1c0c49bfd9c2238515078289908de84a9
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-07-08 14:04:53 -0500

    Support absolute and relative directories for cache parameter

--- src/scripts/gen-cache-checksum.sh
+++ src/scripts/gen-cache-checksum.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-find . -name "pom.xml" | sort | xargs cat > "$PARAM_CHECKSUM_DIR"
+if [[ "$PARAM_CHECKSUM_DIR" == /* ]]; then 
+    CHECKSUM_DIR="$PARAM_CHECKSUM_DIR"
+else
+    CHECKSUM_DIR="${PWD%/"$PARAM_APP_SRC_DIR"}/$PARAM_CHECKSUM_DIR"
+fi
+find . -name "pom.xml" | sort | xargs cat > "$CHECKSUM_DIR"

89a55cd05a1c0e290c0e339d1297bdfec9b38bf7
marboledacci <mateo.arboleda@circleci.com>
2025-07-09 10:40:43 -0500

    Fix problems with cache on windows (#53)
    * Add windows and mac tests
    
    * Update windows image
    
    * install maven on windows and macos
    
    * Save cache to local dir
    
    * Update checksum directory
    
    * Set maven path on windows for tests
    
    * Fix version for path
    
    * Use bash env for path
    
    * use variable for checksum
    
    * Fix typo
    
    * update test
    
    * Support absolute and relative directories for cache parameter
    
    * Fix behavior of root directory on windows
    
    * improve find command

--- src/scripts/gen-cache-checksum.sh
+++ src/scripts/gen-cache-checksum.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
-find . -name 'pom.xml' | sort | xargs cat > /tmp/maven_cache_seed
+detect_os() {
+  detected_platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
+
+  case "$detected_platform" in
+    linux*)
+      PLATFORM=linux
+      ;;
+    darwin*)
+      PLATFORM=macos
+      ;;
+    msys*|cygwin*)
+      PLATFORM=windows
+      ;;
+    *)
+      printf '%s\n' "Unsupported OS: \"$detected_platform\"."
+      exit 1
+      ;;
+  esac
+}
+
+detect_os
+
+if [[ "$PARAM_CHECKSUM_DIR" == /* ]]; then
+    if [[ "$PLATFORM" == "windows" ]]; then
+        CHECKSUM_DIR="/c$PARAM_CHECKSUM_DIR"
+    else
+        CHECKSUM_DIR="$PARAM_CHECKSUM_DIR"
+    fi
+
+else
+    CHECKSUM_DIR="${PWD%/"$PARAM_APP_SRC_DIR"}/$PARAM_CHECKSUM_DIR"
+fi
+find . -name .git -prune -o -name 'pom.xml' -print0 | sort -z | xargs -0 cat > "$CHECKSUM_DIR"
